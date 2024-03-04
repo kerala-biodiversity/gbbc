8e68715e41f6bc44cd39800588c4dec1a832b61d
Athul R T <35709823+athulvis@users.noreply.github.com>
2024-03-04 11:06:48 +0530

    Update filter_config.R

--- R_slicer/filter_config.R
+++ R_slicer/filter_config.R
@@ -1,5 +1,5 @@
 #ebird_file <- "/home/user/ebird/ebdfile.txt"
 ebird_file <- "/home/user/ebird/filename.txt"
 # start date and end date in yyyy-mm-dd format
-start_date="2021-12-03"
-end_date="2021-12-06"
+start_date <- "2021-12-03"
+end_date <- "2021-12-06"
